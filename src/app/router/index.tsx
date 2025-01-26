0c21a5512dc512b2c32c369823a665d799f3471d
Nikita M <iamlegkiy@gmail.com>
2025-01-26 11:23:05 +0200

    done

--- src/app/router/index.tsx
+++ src/app/router/index.tsx
@@ -1,27 +1,33 @@
 import { RouteObject, createHashRouter } from 'react-router';
 import * as Pages from '@/pages';
 import { LayoutWithNavbar } from '@/widgets';
+import ErrorDisplay from '@/shared/ui/ErrorDisplay';
 
 const routes: RouteObject[] = [
   {
-    index: true,
-    path: '/',
-    element: <Pages.Main />,
-  },
-  {
-    path: '/project',
-    Component: LayoutWithNavbar,
+    element: <ErrorDisplay />,
     children: [
       {
         index: true,
-        path: ':id',
-        element: <Pages.ProjectId />,
+        path: '/',
+        element: <Pages.Main />,
+      },
+      {
+        path: '/project',
+        Component: LayoutWithNavbar,
+        children: [
+          {
+            index: true,
+            path: ':id',
+            element: <Pages.ProjectId />,
+          },
+        ],
+      },
+      {
+        path: '/settings',
+        element: <Pages.Settings />,
       },
     ],
-  },
-  {
-    path: '/settings',
-    element: <Pages.Settings />,
   },
 ];
 
