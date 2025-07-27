2e3fcc2b4a9446eedaeae7e501c54e87e9c6aa3b
Nikita Maxakov <iamlegkiy@gmail.com>
2025-07-27 13:19:46 +0200

    delete project

---
+++ src/entities/Project/ui/DeleteProjectModal.tsx
@@ -0,0 +1,38 @@
+import { Button, Stack } from '@mui/material';
+import DeleteRounded from '@mui/icons-material/DeleteRounded';
+import { ConfirmModal, Text } from '@/shared/ui';
+import { useProjectStore } from '../lib';
+
+interface Props {
+  projectId: string;
+  projectName: string;
+}
+const DeleteProjectModal = ({ projectId, projectName }: Props) => {
+  const deleteProject = useProjectStore((e) => e.deleteProject);
+
+  const handleDelete = () => {
+    deleteProject(projectId);
+  };
+
+  return (
+    <ConfirmModal
+      onConfirm={handleDelete}
+      title={
+        <>
+          <Text mess="project.deleteConfirm" text /> <strong>«{projectName}»</strong>?
+        </>
+      }
+      warningMess={
+        <Stack gap={2}>
+          <Text mess={['project.deleteWarn', '!']} />
+        </Stack>
+      }
+    >
+      <Button size="small" variant="square" color="error">
+        <DeleteRounded fontSize="small" />
+      </Button>
+    </ConfirmModal>
+  );
+};
+
+export default DeleteProjectModal;
